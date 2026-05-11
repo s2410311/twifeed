2a18458bd869583bee267913178e019263bdfd39
“s2410311” <s2410311@u.tsukuba.ac.jp>
2026-05-11 20:06:19 +0900

    fix bug

--- db/schema.sql
+++ db/schema.sql
@@ -21,6 +21,6 @@ CREATE TABLE IF NOT EXISTS passkeys(
 CREATE TABLE follows (
     follower_uid TEXT NOT NULL,
     followee_uid TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(follower_uid, followee_uid)
 );
