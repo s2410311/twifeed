e972b9dd43349370ce240bda944c2f18c83a9bb8
Yoshi553 <s2410385@klis.tsukuba.ac.jp>
2026-06-04 13:05:35 +0900

    Delete background directory

--- background/follow.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE follows (
-    follower_uid TEXT NOT NULL,
-    followee_uid TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    UNIQUE(follower_uid, followee_uid)
-);
